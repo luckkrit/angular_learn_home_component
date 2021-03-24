82ea8a78412bf7e729597f5630fea0de709f3c11
Krit Chomaitong <luckkrit@hotmail.com>
2021-03-24 11:15:48 +0700

    add Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { CountingComponent } from './counting/counting.component';
+import { Lab4Component } from './lab4/lab4.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { SearchbarComponent } from './home/navbar/searchbar/searchbar.component';
+import { TopbannerComponent } from './home/topbanner/topbanner.component';
+import { CategorymenuComponent } from './home/categorymenu/categorymenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountingComponent,
+    Lab4Component,
+    HomeComponent,
+    NavbarComponent,
+    SearchbarComponent,
+    TopbannerComponent,
+    CategorymenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
