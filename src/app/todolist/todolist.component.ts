0cf7ae99577f7c2e8b8666e7ff1500533751ee10
Krit Chomaitong <luckkrit@hotmail.com>
2021-03-24 22:59:58 +0700

    update Todolist by add delete button.

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -3,19 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent implements OnInit {
-
   tasks: string[];
+
   constructor() {
     this.tasks = [];
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
+
   addTask(task: string): void {
     this.tasks.push(task);
   }
 
+  deleteTask(i: number): void {
+    this.tasks.splice(i, 1);
+  }
 }
